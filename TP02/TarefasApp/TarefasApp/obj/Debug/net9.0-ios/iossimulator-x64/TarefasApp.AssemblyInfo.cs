222064497d6a44976df6f3aae6c82e007b6664ec
CesarBeda <cesarbedacandido@hotmail.com>
2025-10-30 17:59:28 -0300

    TO02

--- TP02/TarefasApp/TarefasApp/obj/Debug/net9.0-ios/iossimulator-x64/TarefasApp.AssemblyInfo.cs
+++ TP02/TarefasApp/TarefasApp/obj/Debug/net9.0-ios/iossimulator-x64/TarefasApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TarefasApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+818c70b13646f33e5bfe5fabe44898b3c384eb1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b26f63222ee8d87a6b690abe47d59e41e9fb5e72")]
 [assembly: System.Reflection.AssemblyProductAttribute("TarefasApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TarefasApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
